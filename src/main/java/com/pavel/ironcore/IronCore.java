87bd2f7caa3e74aadfed45c1c30e923b83baaae0
avelNet <avelnet@outlook.com>
2026-06-05 20:34:58 +0700

    Switch to standard Forge 1.20.1 for TLauncher compatibility

--- src/main/java/com/pavel/ironcore/IronCore.java
+++ src/main/java/com/pavel/ironcore/IronCore.java
@@ -9,9 +9,7 @@ import com.pavel.ironcore.network.ModMessages;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -31,6 +29,8 @@ public class IronCore {
         
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::clientSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -41,30 +41,17 @@ public class IronCore {
         KeyBindings.init();
     }
 
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
-            event.register(KeyBindings.flamethrowerKey);
-        }
-
-        @SubscribeEvent
-        public static void onRegisterOverlays(RegisterGuiOverlaysEvent event) {
-            event.registerAboveAll("suit_hud", SuitHUD.HUD_OVERLAY);
+    @SubscribeEvent
+    public void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof Player) {
+            if (!event.getObject().getCapability(SuitCapabilityProvider.SUIT_CAPABILITY).isPresent()) {
+                event.addCapability(new ResourceLocation(MODID, "suit_properties"), new SuitCapabilityProvider());
+            }
         }
     }
 
-    @Mod.EventBusSubscriber(modid = MODID)
-    public static class ForgeEvents {
-        @SubscribeEvent
-        public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
-            if (event.getObject() instanceof Player) {
-                if (!event.getObject().getCapability(SuitCapabilityProvider.SUIT_CAPABILITY).isPresent()) {
-                    event.addCapability(new ResourceLocation(MODID, "suit_properties"), new SuitCapabilityProvider());
-                }
-            }
-        }
-
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ModEvents {
         @SubscribeEvent
         public static void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
             event.register(SuitCapability.class);
--- src/main/java/com/pavel/ironcore/client/SuitHUD.java
+++ src/main/java/com/pavel/ironcore/client/SuitHUD.java
@@ -3,30 +3,38 @@ package com.pavel.ironcore.client;
 import com.pavel.ironcore.capability.SuitCapabilityProvider;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import com.pavel.ironcore.IronCore;
+import net.minecraftforge.api.distmarker.Dist;
 
+@Mod.EventBusSubscriber(modid = IronCore.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class SuitHUD {
-    public static final IGuiOverlay HUD_OVERLAY = (gui, guiGraphics, partialTick, width, height) -> {
-        Minecraft minecraft = Minecraft.getInstance();
-        if (minecraft.player == null) return;
+    @SubscribeEvent
+    public static void onRegisterOverlays(RegisterGuiOverlaysEvent event) {
+        event.registerAboveAll("suit_hud", (gui, guiGraphics, partialTick, width, height) -> {
+            Minecraft minecraft = Minecraft.getInstance();
+            if (minecraft.player == null) return;
 
-        minecraft.player.getCapability(SuitCapabilityProvider.SUIT_CAPABILITY).ifPresent(suit -> {
-            if (suit.getSuitTier().equals("none")) return;
+            minecraft.player.getCapability(SuitCapabilityProvider.SUIT_CAPABILITY).ifPresent(suit -> {
+                if (suit.getSuitTier().equals("none")) return;
 
-            int x = 10;
-            int y = 10;
+                int x = 10;
+                int y = 10;
 
-            String energyText = "Energy: " + suit.getEnergy() + " / " + suit.getMaxEnergy() + " FE";
-            String tierText = "Suit: " + suit.getSuitTier().toUpperCase();
-            String durabilityText = "Armor: " + suit.getFrameDurability() + "%";
+                String energyText = "Energy: " + suit.getEnergy() + " / " + suit.getMaxEnergy() + " FE";
+                String tierText = "Suit: " + suit.getSuitTier().toUpperCase();
+                String durabilityText = "Armor: " + suit.getFrameDurability() + "%";
 
-            guiGraphics.drawString(minecraft.font, tierText, x, y, 0xFFFFFF);
-            guiGraphics.drawString(minecraft.font, energyText, x, y + 10, 0x00AAFF);
-            guiGraphics.drawString(minecraft.font, durabilityText, x, y + 20, 0xFFAA00);
-            
-            if (suit.getPalladiumPoisoning() > 0) {
-                guiGraphics.drawString(minecraft.font, "POISONING: " + suit.getPalladiumPoisoning(), x, y + 30, 0xFF0000);
-            }
+                guiGraphics.drawString(minecraft.font, tierText, x, y, 0xFFFFFF);
+                guiGraphics.drawString(minecraft.font, energyText, x, y + 10, 0x00AAFF);
+                guiGraphics.drawString(minecraft.font, durabilityText, x, y + 20, 0xFFAA00);
+                
+                if (suit.getPalladiumPoisoning() > 0) {
+                    guiGraphics.drawString(minecraft.font, "POISONING: " + suit.getPalladiumPoisoning(), x, y + 30, 0xFF0000);
+                }
+            });
         });
-    };
+    }
 }
