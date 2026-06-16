b74740acb67b06c1383c6e00d2b0e54ebcad99f7
avelNet <avelnet@outlook.com>
2026-06-17 01:46:40 +0700

    Revert "tune: faster MK2 overall, faster vertical ascent on both tiers"
    This reverts commit 1f5d5b8173f417fdd281c1f6fabe2974a7e8568e.

--- src/main/java/com/pavel/ironcore/flight/FlightConfig.java
+++ src/main/java/com/pavel/ironcore/flight/FlightConfig.java
@@ -21,9 +21,9 @@ public record FlightConfig(
         double diveTerminalSpeed
 ) {
     public static final FlightConfig MK2 = new FlightConfig(
-            0.95, 0.15, 0.4,
-            0.95, 0.0,
-            1.5, 0.25, 1.5,
+            0.85, 0.15, 0.0,
+            0.85, 0.0,
+            1.5, 0.25, 1.0,
             0.278, 0.85,
             1.3,
             0.12, 1.6
@@ -32,7 +32,7 @@ public record FlightConfig(
     public static final FlightConfig MK3 = new FlightConfig(
             1.0, 0.15, 0.6,
             1.25, 0.7,
-            1.7, 0.2, 1.5,
+            1.2, 0.1, 1.4,
             0.3, 0.85,
             1.45,
             0.15, 2.2
